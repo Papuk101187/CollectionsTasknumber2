59b22cabd5e9546c7eeff86a3f177855b65020b4
Papuk101187 <42898446+Papuk101187@users.noreply.github.com>
2021-01-08 22:27:57 +0200

    fiks

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,12 @@ public class Main {
 
     public static void main(String args[]) {
 
-        String string = "3806336673783809756672834545380634372343380633667378380987874536380667443723380634372343";
+        String string = "+380633667378 380633667378 80633667378 0633667378 380634741526 0634741526" +
+                "380936961527";
+
         PersonalComputer personalComputer = new PersonalComputer(new Parsing());
         personalComputer.cdrom(string);
         personalComputer.checkRecord();
-
 
     }
 
--- src/Parsing.java
+++ src/Parsing.java
@@ -5,27 +5,45 @@ import java.util.regex.Pattern;
 public class Parsing implements Programs {
 
 
-
-
     public Collection starting(String string) {
 
 
         Set<String> myHashSet = new HashSet<String>();
 
-        Pattern pattern = Pattern.compile("(38\\d{10})");
+        Pattern pattern = Pattern.compile("(\\+38\\d{10}|38\\d{10}|0\\d{9}|8\\d{10})");
         Matcher matches = pattern.matcher(string);
 
-        while (matches.find()){
-            myHashSet.add(matches.group());
+        while (matches.find()) {
+            String phone = Getcorrectphone(matches.group());
+            myHashSet.add(phone);
         }
 
         return myHashSet;
     }
 
+    private String Getcorrectphone(String group) {
 
+        int lengs = group.length();
+        String phone = group;
 
+        switch (lengs) {
 
+            case (13):
+                break;
+            case (12):
+                phone = "+" + phone;
+                break;
+            case (11):
+                phone = "+3" + phone;
+                break;
+            case (10):
+                phone = "+38" + phone;
+                break;
+        }
 
+        return phone;
+
+    }
 
 
 }
